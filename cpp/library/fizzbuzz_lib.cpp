ed050bb89e100f8e6a28e40ed7f7a039479297a0
Butch Howard <butchhoward@gmail.com>
2019-12-14 16:10:43 -0600

    no ifs using a map

--- cpp/library/fizzbuzz_lib.cpp
+++ cpp/library/fizzbuzz_lib.cpp
@@ -1,27 +1,45 @@
 #include "fizzbuzz_lib.h"
 
 #include <sstream>
+#include <map>
 
 using namespace fizzbuzzlib;
 
+class Fizzbuzzer {
+public:
+    Fizzbuzzer()
+    {
+        for (int i = 1; i < 100; i++)
+        {
+            std::stringstream s;
+            s << i;
+            fizzmap[i] = s.str();
+        }
+        for (int i = 3; i < 100; i += 3)
+        {
+            fizzmap[i] = "fizz";
+        }
+        for (int i = 5; i < 100; i += 5)
+        {
+            fizzmap[i] = "buzz";
+        }
+        for (int i = 15; i < 100; i += 15)
+        {
+            fizzmap[i] = "fizzbuzz";
+        }
+    }
+
+    const std::string& operator[](int v)
+    {
+        return fizzmap[v];
+    }
+
+private:
+    std::map<int, std::string> fizzmap;
+};
+
 std::string fizzbuzzlib::fizzbuzz(const int value)
 {
-    if (value % 15 == 0)
-    {
-        return "fizzbuzz";
-    }
-    else if ( value % 3 == 0)
-    {
-        return "fizz";
-    }
-    else if (value % 5 == 0)
-    {
-        return "buzz";
-    }
-    else
-    {
-        std::stringstream s;
-        s << value;
-        return s.str();
-    }
+    Fizzbuzzer fizzer;
+    return fizzer[value];
 }
